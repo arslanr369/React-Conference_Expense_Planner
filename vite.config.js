995b970526ee2161472024a4185cab69e043f551
Arslanr369 <Arslanriaz785@gmail.com>
2024-09-07 10:06:54 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React-Conference_Expense_Planner/', // Replace with your repository name
   plugins: [react()],
-})
+});
